b71e4e883f56fec1d398fe515f6d1b657017ed9b
Bradley Beddoes <bradleybeddoes@gmail.com>
2012-11-02 17:14:32 +1000

    Subject: Password verification logic for NIST 800-63-1 Level 2.
    Implements extensive server side checking for Subject passwords to ensure they meet requirements for VHR to be able to assert a NIST level 2 compliant authentication event.

---
+++ virtualhomeregistry/grails-app/services/aaf/vhr/PasswordValidationService.groovy
@@ -0,0 +1,128 @@
+package aaf.vhr
+
+import edu.vt.middleware.password.*
+import edu.vt.middleware.dictionary.*
+import edu.vt.middleware.dictionary.sort.*
+
+class PasswordValidationService {
+  boolean transactional = true
+  def grailsApplication
+
+  ArrayWordList awl
+  Properties msgs
+
+  public PasswordValidationService() {
+    def readers = new java.io.Reader[1]
+    readers[0] = new InputStreamReader(getClass().getResourceAsStream('dictionary-web2.txt'))
+    awl = WordLists.createFromReader(readers, false, new ArraysSort());
+
+    msgs = new Properties()
+    msgs.load(getClass().getResourceAsStream("passwordvalidation.txt"))
+  }
+
+  /*
+    @pre: The ManagedSubject plainPassword and plainPasswordConfirm
+    values have been populated with a plain text representation
+    of the password attempting to be set by the subject
+
+    NIST 800-63-1
+    http://csrc.nist.gov/publications/nistpubs/800-63-1/SP-800-63-1.pdf
+    Memorized Secret Token
+    Level 2
+
+    The memorized secret may be a randomly generated PIN consisting 
+    of 6 or more digits, a user generated string consisting of 8 or 
+    more characters chosen from an alphabet of 90 or more characters, 
+    or a secret with equivalent entropy.
+
+    CSP implements dictionary or composition rule to constrain 
+    usergenerated secrets.
+
+    8 char dict + composition = 30 entropy - Table A.1
+    16 char = 30 entropy - Table A.1
+
+    Thus we enforce:
+      All passwords:
+      Do not contain the login name
+      Minimum length 8 char
+      No whitespace
+      No alphabetic sequences
+      No numerical sequences
+      No qwerty sequences
+      No more then 3 repeat characters
+
+      Password is 8 - 15 char:
+      At least 1 number
+      At least 1 Non Alpha
+      At least 1 Uppercase Character
+      At least 1 Lowercase Chatacter
+      No dictionary words (dictionary file contains 235924 words)
+
+      Password is 16 char or greater:
+      Minimum length 16 char
+  */
+  def validate(ManagedSubject subject) {
+    if(subject.plainPassword != subject.plainPasswordConfirmation) {
+      log.warn("The submitted plaintext passwords for $subject don't match, rejecting.")
+      return [false, ['aaf.vhr.passwordvalidationservice.notmatching']]
+    }
+
+    if(subject.plainPassword.toLowerCase().contains(subject.login.toLowerCase())) {
+      log.warn("The submitted plaintext passwords for $subject don't match, rejecting.")
+      return [false, ['aaf.vhr.passwordvalidationservice.containslogin']]
+    }
+
+    def pw = subject.plainPassword
+
+    MessageResolver resolver = new MessageResolver(msgs)
+
+    WhitespaceRule whitespaceRule = new WhitespaceRule()
+    AlphabeticalSequenceRule alphaSeqRule = new AlphabeticalSequenceRule(3, false)
+    NumericalSequenceRule numSeqRule = new NumericalSequenceRule(3, false)
+    QwertySequenceRule qwertySeqRule = new QwertySequenceRule(3, false)
+    RepeatCharacterRegexRule repeatRule = new RepeatCharacterRegexRule(3)
+
+    List<Rule> ruleList = new ArrayList<Rule>()
+    ruleList.add(whitespaceRule)
+    ruleList.add(alphaSeqRule)
+    ruleList.add(numSeqRule)
+    ruleList.add(qwertySeqRule)
+    ruleList.add(repeatRule)
+
+    if(pw.length() < 16) {
+      LengthRule lengthRule = new LengthRule()
+      lengthRule.minimumLength = 8
+
+      CharacterCharacteristicsRule charRule = new CharacterCharacteristicsRule()
+      charRule.getRules().add(new DigitCharacterRule(1))
+      charRule.getRules().add(new NonAlphanumericCharacterRule(1))
+      charRule.getRules().add(new UppercaseCharacterRule(1))
+      charRule.getRules().add(new LowercaseCharacterRule(1))
+      charRule.setNumberOfCharacteristics(4)
+
+      WordListDictionary dict = new WordListDictionary(awl);
+      DictionarySubstringRule dictRule = new DictionarySubstringRule(dict)
+      dictRule.setWordLength(4)
+      dictRule.setMatchBackwards(true)
+
+      ruleList.add(lengthRule)
+      ruleList.add(charRule)
+      ruleList.add(dictRule)
+    } else {
+      LengthRule lengthRule = new LengthRule()
+      lengthRule.minimumLength = 16
+
+      ruleList.add(lengthRule)
+    }
+
+    PasswordValidator validator = new PasswordValidator(resolver, ruleList)
+    PasswordData passwordData = new PasswordData(new Password(pw))
+
+    RuleResult result = validator.validate(passwordData)
+
+    if(result.isValid())
+      [true, null]
+    else
+      [false, validator.getMessages(result)]
+  }
+}
---
+++ virtualhomeregistry/test/unit/aaf/vhr/PasswordValidationServiceSpec.groovy
@@ -0,0 +1,248 @@
+package aaf.vhr
+
+import grails.test.mixin.*
+import grails.buildtestdata.mixin.Build
+import spock.lang.*
+import grails.plugin.spock.*
+
+import aaf.vhr.ManagedSubject
+
+@TestFor(aaf.vhr.ManagedSubject)
+@Build([ManagedSubject])
+class PasswordValidationServiceSpec extends UnitSpec {
+
+  def pv
+
+  def setup() {
+    pv = new PasswordValidationService(grailsApplication: grailsApplication)
+  }
+
+  def 'confirm passwords submitted must match each other'() {
+    setup:
+    def subject = ManagedSubject.build()
+    subject.plainPassword = val
+    subject.plainPasswordConfirmation = val + "abcdefg"
+
+    when:
+    def result = pv.validate(subject)
+
+    then:
+    !result[0]
+    result[1][0] == 'aaf.vhr.passwordvalidationservice.notmatching'
+
+    where:
+      val << ['Ab1!deXgh', 'Ab1!deXghFBVGs183TrhiEsFtWCV3X4lp0og', 'abcd', 'a']
+  }
+
+  def 'confirm passwords submitted must not contain the login name'() {
+    setup:
+    def subject = ManagedSubject.build()
+    subject.login = 'harry'
+    subject.plainPassword = val
+    subject.plainPasswordConfirmation = val
+
+    when:
+    def result = pv.validate(subject)
+
+    then:
+    !result[0]
+    result[1][0] == 'aaf.vhr.passwordvalidationservice.containslogin'
+
+    where:
+      val << ['Ab1!deXgharry', 'Ab1!deXghFBVGs183TrhiEsFtWCV3X4lp0ogharry', 'abcdharry', 'aharry']
+  }
+
+  def 'confirm password meets NIST minimum of 8 char for user generated pw'() {
+    setup:
+    def subject = ManagedSubject.build()
+    subject.plainPassword = val
+    subject.plainPasswordConfirmation = val
+
+    when:
+    def result = pv.validate(subject)
+
+    then:
+    result[0] == expected
+
+    if(!expected) {
+      assert result[1].size() >= 1
+      assert result[1].contains('aaf.vhr.passwordvalidationservice.toshort')
+    }
+
+    where:
+      val << ['Ab1!deXgh', 'Ab1!deXghFBVGs183TrhiEsFtWCV3X4lp0og', 'abcd', 'a', '']
+      expected << [true, true, false, false, false]
+  }
+
+  def 'confirm password meets non whitespace rules'() {
+    setup:
+    def subject = ManagedSubject.build()
+    subject.plainPassword = val
+    subject.plainPasswordConfirmation = val
+
+    when:
+    def result = pv.validate(subject)
+
+    then:
+    result[0] == expected
+
+    if(!expected) {
+      assert result[1].size() >= 1
+      assert result[1].contains('aaf.vhr.passwordvalidationservice.whitespace')
+    }
+
+    where:
+      val << ['Ab1!deXgh', 'Ab1! deXgh', 'hello there cat tortoise man', 'hellotherecattortoiseman']
+      expected << [true, false, false, true]
+  }
+
+  def 'confirm password meets alphabetical sequence rules'() {
+    setup:
+    def subject = ManagedSubject.build()
+    subject.plainPassword = val
+    subject.plainPasswordConfirmation = val
+
+    when:
+    def result = pv.validate(subject)
+
+    then:
+    result[0] == expected
+
+    if(!expected) {
+      assert result[1].size() >= 1
+      assert result[1].contains('aaf.vhr.passwordvalidationservice.sequence')
+    }
+
+    where:
+      val << ['Ab1!deXgh', 'Abc1!deXgh', 'Abd1!defgXgh', 'Abd1!DEFGXgh']
+      expected << [true, false, false, false]
+  }
+
+  def 'confirm password meets numerical sequence rules'() {
+    setup:
+    def subject = ManagedSubject.build()
+    subject.plainPassword = val
+    subject.plainPasswordConfirmation = val
+
+    when:
+    def result = pv.validate(subject)
+
+    then:
+    result[0] == expected
+
+    if(!expected) {
+      assert result[1].size() >= 1
+      assert result[1].contains('aaf.vhr.passwordvalidationservice.sequence')
+    }
+
+    where:
+      val << ['Ab1!deXgh', 'Ab12!deXgh', 'Ab123!deXgh', 'Ab1!deXgh678']
+      expected << [true, true, false, false]
+  }
+
+  def 'confirm password meets qwerty sequence rules'() {
+    setup:
+    def subject = ManagedSubject.build()
+    subject.plainPassword = val
+    subject.plainPasswordConfirmation = val
+
+    when:
+    def result = pv.validate(subject)
+
+    then:
+    result[0] == expected
+
+    if(!expected) {
+      assert result[1].size() >= 1
+      assert result[1].contains('aaf.vhr.passwordvalidationservice.sequence')
+    }
+
+    where:
+      val << ['Ab1!deXgh', 'Ab1!deXqW', 'Ab1!deXghqWe', 'Ab1!deXghxcVbNm']
+      expected << [true, true, false, false]
+  }
+
+  def 'confirm password meets repeat character sequence rules'() {
+    setup:
+    def subject = ManagedSubject.build()
+    subject.plainPassword = val
+    subject.plainPasswordConfirmation = val
+
+    when:
+    def result = pv.validate(subject)
+
+    then:
+    result[0] == expected
+
+    if(!expected) {
+      assert result[1].size() >= 1
+      assert result[1].contains('aaf.vhr.passwordvalidationservice.match')
+    }
+
+    where:
+      val << ['Ab1!deXgh', 'Aaab1!deXgh', 'Aaaab1!deXgh', 'Ab1!deeeeeeXgh']
+      expected << [true, true, false, false]
+  }
+
+  def 'confirm short password meets char characteristic rules'() {
+    setup:
+    def subject = ManagedSubject.build()
+    subject.plainPassword = val
+    subject.plainPasswordConfirmation = val
+
+    when:
+    def result = pv.validate(subject)
+
+    then:
+    result[0] == expected
+
+    if(!expected) {
+      assert result[1].size() >= 1
+      assert result[1].contains('aaf.vhr.passwordvalidationservice.insufficent.characteristics')
+    }
+
+    where:
+      val << ['Abb!deXgh', 'Ab1ddeXgh', 'ab1!dexgh', 'AB1!DEXGH', 'Ab1!deXgh']
+      expected << [false, false, false, false, true]
+  }
+
+  def 'confirm short password meets no dictionary word rules'() {
+    setup:
+    def subject = ManagedSubject.build()
+    subject.plainPassword = val
+    subject.plainPasswordConfirmation = val
+
+    when:
+    def result = pv.validate(subject)
+
+    then:
+    result[0] == expected
+
+    if(!expected) {
+      assert result[1].size() >= 1
+      assert result[1].contains('aaf.vhr.passwordvalidationservice.dictionary.word')
+    }
+
+    where:
+      val << ['Ab1!deXgh', 'Ab1!password', 'Abraham1!deXgh', 'Ab1!Xylic', 'Ab1!ZEBRA']
+      expected << [true, false, false, false, false]
+  }
+
+  def 'confirm longer password (16+) is allowed dictionary words and does not need to meet char characteristics'() {
+    setup:
+    def subject = ManagedSubject.build()
+    subject.plainPassword = val
+    subject.plainPasswordConfirmation = val
+
+    when:
+    def result = pv.validate(subject)
+
+    then:
+    result[0] == expected
+
+    where:
+      val << ['thehorseZEBRAlikedIceland', 'icelandisagreatplacetovisit', 'ICELANDwasagreatexperience!', 'pizzlepixythermosvenosity']
+      expected << [true, true, true, true]
+  }
+
+}
