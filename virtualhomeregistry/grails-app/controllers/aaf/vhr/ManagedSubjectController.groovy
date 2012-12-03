8731ddd01ed5c637a818f27f0040e5496a872591
Bradley Beddoes <bradleybeddoes@gmail.com>
2012-12-03 11:15:43 +1000

    ManagedSubject: Scaffolded and refined controller code.
    After consultation with AAF team this code allows group administrators to edit ManagedSubject's PII after they are created.

---
+++ virtualhomeregistry/grails-app/controllers/aaf/vhr/ManagedSubjectController.groovy
@@ -0,0 +1,192 @@
+package aaf.vhr
+
+import org.springframework.dao.DataIntegrityViolationException
+import org.apache.shiro.SecurityUtils
+
+class ManagedSubjectController {
+
+  static defaultAction = "list"
+  static allowedMethods = [save: "POST", update: "POST", delete: "DELETE"]
+
+  def beforeInterceptor = [action: this.&validManagedSubject, except: ['list', 'create', 'save']]
+
+  def managedSubjectService
+  def sharedTokenService
+
+  def list() {
+    log.info "Action: list, Subject: $subject"
+    [managedSubjectInstanceList: ManagedSubject.list(params), managedSubjectInstanceTotal: ManagedSubject.count()]
+  }
+
+  def show(Long id) {
+    log.info "Action: show, Subject: $subject"
+    def managedSubjectInstance = ManagedSubject.get(id)
+    [managedSubjectInstance: managedSubjectInstance]
+  }
+
+  def create() {
+    if(validGroup()) {
+      if(SecurityUtils.subject.isPermitted("app:manage:group:${params.group.id}:managedsubject:create")) {
+        log.info "Action: create, Subject: $subject"
+        def group = Group.get(params.group.id)
+        def managedSubjectInstance = new ManagedSubject(group:group, organization:group.organization)
+        [managedSubjectInstance: managedSubjectInstance]
+      }
+      else {
+        log.warn "Attempt to do administrative ManagedSubject create by $subject was denied - not permitted by assigned permissions"
+        response.sendError 403
+      }
+    }
+  }
+
+  def save() {
+    if(validGroup()) {
+      if(SecurityUtils.subject.isPermitted("app:manage:group:${params.group.id}:managedsubject:create")) {
+        def group = Group.get(params.group.id)
+        def managedSubjectInstance = new ManagedSubject()
+        bindData(managedSubjectInstance, params, [include: ['cn', 'email', 'displayName', 'eduPersonAssurance', 'eduPersonAffiliation']])
+        managedSubjectInstance.group = group
+        managedSubjectInstance.organization = group.organization
+        sharedTokenService.generate(managedSubjectInstance)
+
+        if (!managedSubjectInstance.validate()) {
+          flash.type = 'error'
+          flash.message = 'controllers.aaf.vhr.managedsubject.validate.failed'
+          render(view: "create", model: [managedSubjectInstance: managedSubjectInstance])
+          return
+        }
+
+        managedSubjectService.register(managedSubjectInstance)
+
+        log.info "Action: save, Subject: $subject, Object: $managedSubjectInstance"
+        flash.type = 'success'
+        flash.message = 'controllers.aaf.vhr.managedsubject.save.success'
+        redirect(action: "show", id: managedSubjectInstance.id)
+      }
+      else {
+        log.warn "Attempt to do administrative ManagedSubject save by $subject was denied - not permitted by assigned permissions"
+        response.sendError 403
+      }
+    }
+  }
+
+  def edit(Long id) {
+    def managedSubjectInstance = ManagedSubject.get(id)
+    if(SecurityUtils.subject.isPermitted("app:manage:group:${managedSubjectInstance.group.id}:managedsubject:edit")) {
+      log.info "Action: edit, Subject: $subject, Object: managedSubjectInstance"
+
+      [managedSubjectInstance: managedSubjectInstance]
+    }
+    else {
+      log.warn "Attempt to do administrative ManagedSubject edit by $subject was denied - not permitted by assigned permissions"
+      response.sendError 403
+    }
+  }
+
+  def update(Long id, Long version) {
+    def managedSubjectInstance = ManagedSubject.get(id)
+    if(SecurityUtils.subject.isPermitted("app:manage:group:${managedSubjectInstance.group.id}:managedsubject:edit")) {
+      if (version == null) {
+        flash.type = 'error'
+        flash.message = 'controllers.aaf.vhr.managedsubject.update.noversion'
+        render(view: "edit", model: [managedSubjectInstance: managedSubjectInstance])
+        return
+      }
+
+      if (managedSubjectInstance.version > version) {
+        managedSubjectInstance.errors.rejectValue("version", "controllers.aaf.vhr.managedsubject.update.optimistic.locking.failure")
+        render(view: "edit", model: [managedSubjectInstance: managedSubjectInstance])
+        return
+      }
+
+      bindData(managedSubjectInstance, params, [include: ['cn', 'email', 'displayName', 'eduPersonAssurance', 'eduPersonAffiliation']])
+
+      if (!managedSubjectInstance.save()) {
+        flash.type = 'error'
+        flash.message = 'controllers.aaf.vhr.managedsubject.update.failed'
+        render(view: "edit", model: [managedSubjectInstance: managedSubjectInstance])
+        return
+      }
+
+      log.info "Action: update, Subject: $subject, Object: $managedSubjectInstance"
+      flash.type = 'success'
+      flash.message = 'controllers.aaf.vhr.managedsubject.update.success'
+      redirect(action: "show", id: managedSubjectInstance.id)
+    }
+    else {
+      log.warn "Attempt to do administrative ManagedSubject update by $subject was denied - not permitted by assigned permissions"
+      response.sendError 403
+    }
+  }
+
+  def delete(Long id) {
+    def managedSubjectInstance = ManagedSubject.get(id)
+    if(SecurityUtils.subject.isPermitted("app:manage:group:${managedSubjectInstance.group.id}:managedsubject:delete")) {
+      try {
+        managedSubjectInstance.delete()
+
+        log.info "Action: delete, Subject: $subject, Object: $managedSubjectInstance"
+        flash.type = 'success'
+        flash.message = 'controllers.aaf.vhr.managedsubject.delete.success'
+        redirect(action: "list")
+      }
+      catch (DataIntegrityViolationException e) {
+        flash.type = 'error'
+        flash.message = 'controllers.aaf.vhr.managedsubject.delete.failure'
+        redirect(action: "show", id: id)
+      }
+    }
+    else {
+      log.warn "Attempt to do administrative ManagedSubject delete by $subject was denied - not permitted by assigned permissions"
+      response.sendError 403
+    }
+  }
+
+  private validGroup() {
+    if(!params.group?.id) {
+      log.warn "Group ID was not present"
+
+      flash.type = 'info'
+      flash.message = message(code: 'controllers.aaf.vhr.managedsubject.group.no.id')
+
+      redirect action:'list'
+      return false
+    }
+
+    def groupInstance = Group.get(params.group.id)
+    if (!groupInstance) {
+      log.warn "groupInstance was not a valid instance"
+
+      flash.type = 'info'
+      flash.message = 'controllers.aaf.vhr.managedsubject.group.notfound'
+
+      redirect action:'list'
+      return false
+    }
+
+    true
+  }
+
+  private validManagedSubject() {
+    if(!params.id) {
+      log.warn "ID was not present"
+
+      flash.type = 'info'
+      flash.message = message(code: 'controllers.aaf.vhr.managedsubject.no.id')
+
+      redirect action:'list'
+      return false
+    }
+
+    def managedSubjectInstance = ManagedSubject.get(params.id)
+    if (!managedSubjectInstance) {
+      log.warn "managedSubjectInstance was not a valid instance"
+
+      flash.type = 'info'
+      flash.message = 'controllers.aaf.vhr.managedsubject.notfound'
+
+      redirect action:'list'
+      return false
+    }
+  }
+}
---
+++ virtualhomeregistry/test/unit/aaf/vhr/ManagedSubjectControllerSpec.groovy
@@ -0,0 +1,458 @@
+package aaf.vhr
+
+import grails.test.mixin.*
+import grails.plugin.spock.*
+import grails.buildtestdata.mixin.Build
+
+import spock.lang.*
+
+import test.shared.ShiroEnvironment
+
+@TestFor(aaf.vhr.ManagedSubjectController)
+@Build([ManagedSubject, aaf.base.identity.Subject])
+class ManagedSubjectControllerSpec  extends spock.lang.Specification {
+  
+  @Shared def shiroEnvironment = new ShiroEnvironment()
+
+  aaf.base.identity.Subject subject
+  org.apache.shiro.subject.Subject shiroSubject
+  
+  def cleanupSpec() { 
+    shiroEnvironment.tearDownShiro() 
+  }
+
+  def setup() {
+    subject = aaf.base.identity.Subject.build()
+
+    shiroSubject = Mock(org.apache.shiro.subject.Subject)
+    shiroSubject.id >> subject.id
+    shiroSubject.principal >> subject.principal
+    shiroSubject.isAuthenticated() >> true
+    shiroEnvironment.setSubject(shiroSubject)
+    
+    controller.metaClass.getSubject = { subject }
+  }
+
+  def 'ensure beforeInterceptor only excludes list, create, save'() {
+    when:
+    controller
+
+    then:
+    controller.beforeInterceptor.except.size() == 3
+    controller.beforeInterceptor.except.containsAll(['list', 'create', 'save'])
+  }
+
+  def 'ensure redirect to list if no ID presented to beforeInterceptor'() {
+    when:
+    def result = controller.validManagedSubject()
+
+    then:
+    !result
+    response.status == 302
+
+    response.redirectedUrl == "/managedSubject/list"
+
+    flash.type == 'info'
+    flash.message == 'controllers.aaf.vhr.managedsubject.no.id'
+  }
+
+  def 'ensure redirect to list if no valid instance found by beforeInterceptor'() {
+    when:
+    params.id = 1
+    def result = controller.validManagedSubject()
+
+    then:
+    !result
+    response.status == 302
+
+    response.redirectedUrl== "/managedSubject/list"
+
+    flash.type == 'info'
+    flash.message == 'controllers.aaf.vhr.managedsubject.notfound'
+  }
+
+  def 'ensure redirect to list if no groupID presented to validGroup'() {
+    when:
+    params.group = [:]
+    def result = controller.validGroup()
+
+    then:
+    !result
+    response.status == 302
+
+    response.redirectedUrl == "/managedSubject/list"
+
+    flash.type == 'info'
+    flash.message == 'controllers.aaf.vhr.managedsubject.group.no.id'
+  }
+
+  def 'ensure redirect to list if no valid instance found by validGroup'() {
+    when:
+    params.group = [id:1]
+    def result = controller.validGroup()
+
+    then:
+    !result
+    response.status == 302
+
+    response.redirectedUrl== "/managedSubject/list"
+
+    flash.type == 'info'
+    flash.message == 'controllers.aaf.vhr.managedsubject.group.notfound'
+  }
+
+  def 'ensure true if no valid group found by validGroup'() {
+    setup:
+    def group = Group.build()
+
+    when:
+    params.group = [id:group.id]
+    def result = controller.validGroup()
+
+    then:
+    result
+  }
+
+  def 'ensure correct output from list'() {
+    setup:
+    (1..10).each { ManagedSubject.build() }
+
+    when:
+    params.max = max
+    def model = controller.list()
+
+    then:
+    ManagedSubject.count() == total
+    model.managedSubjectInstanceList.size() == expectedResult
+
+    where:
+    max | total | expectedResult
+    0 | 10 | 10
+    5 | 10 | 5
+  }
+
+  def 'ensure correct output from show'() {
+    setup:
+    def managedSubjectTestInstance = ManagedSubject.build()
+
+    when:
+    params.id = managedSubjectTestInstance.id
+    def model = controller.show()
+
+    then:
+    model.managedSubjectInstance == managedSubjectTestInstance
+  }
+
+  def 'ensure correct output from create when valid permission'() {
+    setup:
+    def group = Group.build()
+    shiroSubject.isPermitted("app:manage:group:${group.id}:managedsubject:create") >> true
+
+    when:
+    params.group = [id:group.id]
+    def model = controller.create()
+
+    then:
+    model.managedSubjectInstance.instanceOf(ManagedSubject)
+  }
+
+  def 'ensure correct output from create when invalid permission'() {
+    setup:
+    def group = Group.build()
+    shiroSubject.isPermitted("app:manage:group:${group.id + 1}:managedsubject:create") >> true
+
+    when:
+    params.group = [id:group.id]
+    def model = controller.create()
+
+    then:
+    model == null
+    response.status == 403
+  }
+
+  def 'ensure correct output from save when invalid permission'() {
+    setup:
+    def group = Group.build()
+    shiroSubject.isPermitted("app:manage:group:${group.id + 1}:managedsubject:create") >> true
+
+    when:
+    params.group = [id:group.id]
+    def model = controller.save()
+
+    then:
+    model == null
+    response.status == 403
+  }
+
+  def 'ensure correct output from save with invalid data and when valid permission'() {
+    setup:
+    def sharedTokenService = Mock(aaf.vhr.SharedTokenService)
+    def group = Group.build()
+    shiroSubject.isPermitted("app:manage:group:${group.id}:managedsubject:create") >> true
+
+    def managedSubjectTestInstance = ManagedSubject.build(group:group, organization:group.organization)
+    managedSubjectTestInstance.properties.each {
+      if(it.value) {
+        if(grailsApplication.isDomainClass(it.value.getClass()))
+          params."${it.key}" = [id:"${it.value.id}"]
+        else
+          params."${it.key}" = "${it.value}"
+      }
+    }
+    managedSubjectTestInstance.delete()
+
+    ManagedSubject.metaClass.save { null }
+
+    controller.sharedTokenService = sharedTokenService
+    
+    when:
+    controller.save()
+
+    then:
+    1 * sharedTokenService.generate(_ as ManagedSubject)
+
+    ManagedSubject.count() == 0
+    flash.type == 'error'
+    flash.message == 'controllers.aaf.vhr.managedsubject.validate.failed'
+
+    model.managedSubjectInstance.properties.each {
+      it.value == managedSubjectTestInstance.getProperty(it.key)
+    }
+  }
+
+  def 'ensure correct output from save with valid data and when valid permission'() {
+    setup:
+    def sharedTokenService = Mock(aaf.vhr.SharedTokenService)
+    def managedSubjectService = Mock(aaf.vhr.ManagedSubjectService)
+    def group = Group.build()
+    shiroSubject.isPermitted("app:manage:group:${group.id}:managedsubject:create") >> true
+
+    def managedSubjectTestInstance = ManagedSubject.build(group:group, organization:group.organization)
+    managedSubjectTestInstance.properties.each {
+      if(it.value) {
+        if(grailsApplication.isDomainClass(it.value.getClass()))
+          params."${it.key}" = [id:"${it.value.id}"]
+        else
+          params."${it.key}" = "${it.value}"
+      }
+    }
+    managedSubjectTestInstance.delete()
+
+    controller.sharedTokenService = sharedTokenService
+    controller.managedSubjectService = managedSubjectService
+
+    expect:
+    ManagedSubject.count() == 0
+
+    when:
+    controller.save()
+
+    then:
+    1 * sharedTokenService.generate(_ as ManagedSubject) >> {ManagedSubject subject -> subject.sharedToken = '1234'}
+    1 * managedSubjectService.register(_ as ManagedSubject)>> {ManagedSubject subject -> subject.save()}
+    ManagedSubject.count() == 1
+    flash.type == 'success'
+    flash.message == 'controllers.aaf.vhr.managedsubject.save.success'
+
+    def savedManagedSubjectTestInstance = ManagedSubject.first()
+    savedManagedSubjectTestInstance.properties.each {
+      it.value == managedSubjectTestInstance.getProperty(it.key)
+    }
+  }
+
+  def 'ensure correct output from edit when invalid permission'() {
+    setup:
+    def group = Group.build()
+    def managedSubjectTestInstance = ManagedSubject.build(group:group, organization:group.organization)
+    shiroSubject.isPermitted("app:manage:group:${group.id + 1}:managedsubject:edit") >> true
+
+    when:
+    params.id = managedSubjectTestInstance.id
+    def model = controller.edit()
+
+    then:
+    model == null
+    response.status == 403
+  }
+
+  def 'ensure correct output from edit when valid permission'() {
+    setup:
+    def group = Group.build()
+    def managedSubjectTestInstance = ManagedSubject.build(group:group, organization:group.organization)
+    shiroSubject.isPermitted("app:manage:group:${group.id}:managedsubject:edit") >> true
+
+    when:
+    params.id = managedSubjectTestInstance.id
+    def model = controller.edit()
+
+    then:
+    model.managedSubjectInstance == managedSubjectTestInstance
+  }
+
+  def 'ensure correct output from update when invalid permission'() {
+    setup:
+    def group = Group.build()
+    def managedSubjectTestInstance = ManagedSubject.build(group:group, organization:group.organization)
+    shiroSubject.isPermitted("app:manage:group:${group.id + 1}:managedsubject:edit") >> true
+
+    when:
+    params.id = managedSubjectTestInstance.id
+    def model = controller.update()
+
+    then:
+    model == null
+    response.status == 403
+  }
+
+  def 'ensure correct output from update with null version but valid permission'() {
+    setup:
+    def group = Group.build()
+    def managedSubjectTestInstance = ManagedSubject.build(group:group, organization:group.organization)
+    shiroSubject.isPermitted("app:manage:group:${group.id}:managedsubject:edit") >> true
+    
+    expect:
+    ManagedSubject.count() == 1
+
+    when:
+    params.id = managedSubjectTestInstance.id
+    params.version = null
+    controller.update()
+
+    then:
+    ManagedSubject.count() == 1
+    flash.type == 'error'
+    flash.message == 'controllers.aaf.vhr.managedsubject.update.noversion'
+  }
+
+  def 'ensure correct output from update with invalid data and when valid permission'() {
+    setup:
+    def group = Group.build()
+    def managedSubjectTestInstance = ManagedSubject.build(group:group, organization:group.organization)
+    shiroSubject.isPermitted("app:manage:group:${group.id}:managedsubject:edit") >> true
+    managedSubjectTestInstance.getVersion() >> 20
+    
+    managedSubjectTestInstance.properties.each {
+      if(it.value) {
+        if(grailsApplication.isDomainClass(it.value.getClass()))
+          params."${it.key}" = [id:"${it.value.id}"]
+        else
+          params."${it.key}" = "${it.value}"
+      }
+    }
+    ManagedSubject.metaClass.save { null }
+    
+    expect:
+    ManagedSubject.count() == 1
+
+    when:
+    params.id = managedSubjectTestInstance.id
+    params.version = 1
+    controller.update()
+
+    then:
+    ManagedSubject.count() == 1
+    flash.type == 'error'
+    flash.message == 'controllers.aaf.vhr.managedsubject.update.failed'
+
+    model.managedSubjectInstance.properties.each {
+      it.value == managedSubjectTestInstance.getProperty(it.key)
+    }
+  }
+
+  def 'ensure correct output from update with valid data and when valid permission'() {
+    setup:
+    def group = Group.build()
+    def managedSubjectTestInstance = ManagedSubject.build(group:group, organization:group.organization)
+    shiroSubject.isPermitted("app:manage:group:${group.id}:managedsubject:edit") >> true
+    
+    managedSubjectTestInstance.properties.each {
+      if(it.value) {
+        if(grailsApplication.isDomainClass(it.value.getClass()))
+          params."${it.key}" = [id:"${it.value.id}"]
+        else
+          params."${it.key}" = "${it.value}"
+      }
+    }
+
+    expect:
+    ManagedSubject.count() == 1
+
+    when:
+    params.id = managedSubjectTestInstance.id
+    params.version = 0
+    controller.update()
+
+    then:
+    ManagedSubject.count() == 1
+    flash.type == 'success'
+    flash.message == 'controllers.aaf.vhr.managedsubject.update.success'
+
+    def savedManagedSubjectTestInstance = ManagedSubject.first()
+    savedManagedSubjectTestInstance == managedSubjectTestInstance
+
+    savedManagedSubjectTestInstance.properties.each {
+      it.value == managedSubjectTestInstance.getProperty(it.key)
+    }
+  }
+
+  def 'ensure correct output from delete when invalid permission'() {
+    setup:
+    def group = Group.build()
+    def managedSubjectTestInstance = ManagedSubject.build(group:group, organization:group.organization)
+    shiroSubject.isPermitted("app:manage:group:${group.id + 1}:managedsubject:delete") >> true
+
+    when:
+    params.id = managedSubjectTestInstance.id
+    def model = controller.delete()
+
+    then:
+    model == null
+    response.status == 403
+  }
+
+  def 'ensure correct output from delete when valid permission'() {
+    setup:
+    def group = Group.build()
+    def managedSubjectTestInstance = ManagedSubject.build(group:group, organization:group.organization)
+    shiroSubject.isPermitted("app:manage:group:${group.id}:managedsubject:delete") >> true
+
+    expect:
+    ManagedSubject.count() == 1
+
+    when:
+    params.id = managedSubjectTestInstance.id
+    def model = controller.delete()
+
+    then:
+    ManagedSubject.count() == 0
+
+    response.redirectedUrl == "/managedSubject/list"
+
+    flash.type == 'success'
+    flash.message == 'controllers.aaf.vhr.managedsubject.delete.success'
+  }
+
+  def 'ensure correct output from delete when integrity violation'() {
+    setup:
+    def group = Group.build()
+    def managedSubjectTestInstance = ManagedSubject.build(group:group, organization:group.organization)
+    shiroSubject.isPermitted("app:manage:group:${group.id}:managedsubject:delete") >> true
+
+    ManagedSubject.metaClass.delete { throw new org.springframework.dao.DataIntegrityViolationException("Thrown from test case") }
+
+    expect:
+    ManagedSubject.count() == 1
+
+    when:
+    params.id = managedSubjectTestInstance.id
+    def model = controller.delete()
+
+    then:
+    ManagedSubject.count() == 1
+
+    response.redirectedUrl == "/managedSubject/show/${managedSubjectTestInstance.id}"
+
+    flash.type == 'error'
+    flash.message == 'controllers.aaf.vhr.managedsubject.delete.failure'
+  }
+}
