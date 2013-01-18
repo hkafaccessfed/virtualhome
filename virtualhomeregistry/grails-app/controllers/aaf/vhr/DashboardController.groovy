hkafaccessfed/virtualhome

commit a15f82d612197968437c51221d181c87a88440f1

Dashboard: Explicitly show groups for organization user admins.
Previously the dashboard showed the Organizations you administered
and any groups you directly administered.

This change also shows groups which admins have administrative access
over due to the fact that they administer the organization level.

Closes #79.

virtualhomeregistry/grails-app/controllers/aaf/vhr/DashboardController.groovy
@@ -16,6 +16,7 @@ class DashboardController {
   def dashboard = {
     def organizationInstanceList = [] as List
     def groupInstanceList = [] as List
+    def collatedGroupInstanceList = [:]
     def statistics = [:]
       
     use(TimeCategory) {
@@ -25,13 +26,19 @@ class DashboardController {
           if(components.size() == 3) {
             def organization = Organization.get(components[1])
             organizationInstanceList << organization
+
+            organization.groups.each { group ->
+              groupInstanceList << group
+            }
           }
         }
         if(role.name.startsWith('group:')) {
           def components = role.name.split(':')
           if(components.size() == 3) {
             def group = Group.get(components[1])
-            groupInstanceList << group
+            if(!groupInstanceList.contains(group)){
+              groupInstanceList << group
+            }
           }
         }
       }
@@ -55,7 +62,14 @@ class DashboardController {
       statistics.last12MonthSessions = last12MonthSessions
     }
 
-    [organizationInstanceList:organizationInstanceList, groupInstanceList: groupInstanceList, statistics:statistics]
+    groupInstanceList.each {
+      if(collatedGroupInstanceList."${it.organization.displayName}" == null) {
+        collatedGroupInstanceList."${it.organization.displayName}" = [] as List
+      }
+      collatedGroupInstanceList."${it.organization.displayName}" << it
+    }
+
+    [organizationInstanceList:organizationInstanceList, groupInstanceList: collatedGroupInstanceList, statistics:statistics]
   }
 
 }

virtualhomeregistry/grails-app/views/dashboard/dashboard.gsp
@@ -10,12 +10,12 @@
       <div class="span6">
         <div class="well well-small administering">
           <h2><g:message code="views.aaf.vhr.dashboard.myorganizations"/></h2>
-          <g:if test="${organizationInstanceList}">
-            <ol>
+          <g:if test="${organizationInstanceList.sort{it.displayName}}">
+            <ul class="unstyled">
             <g:each in="${organizationInstanceList.sort{it.displayName}}" var="org">
               <li><g:link controller="organization" action="show" id="${org.id}"><g:fieldValue bean="${org}" field="displayName"/></g:link></li>
             </g:each>
-            </ol>
+            </ul>
           </g:if>
           <g:else>
             <p><g:message code="views.aaf.vhr.dashboard.noorganizations"/></p>
@@ -29,11 +29,16 @@
         <div class="well well-small administering">
           <h2><g:message code="views.aaf.vhr.dashboard.mygroups"/></h2>
           <g:if test="${groupInstanceList}">
-            <ol>
-            <g:each in="${groupInstanceList.sort{it.organization.displayName}}" var="group">
-              <li><g:link controller="group" action="show" id="${group.id}"><g:fieldValue bean="${group}" field="name"/></g:link> (<g:fieldValue bean="${group.organization}" field="displayName"/>)</li>
+            
+            <g:each in="${groupInstanceList.sort{it.key}}" var="entry">
+              <strong class="muted">${entry.key}</strong>
+              <ul class="unstyled">
+                <g:each in="${entry.value.sort{it.name}}" var="group">
+                  <li><g:link controller="group" action="show" id="${group.id}"><g:fieldValue bean="${group}" field="name"/></g:link></li>
+                </g:each>
+              </ul>
             </g:each>
-            </ol>
+            
           </g:if>
           <g:else>
             <p><g:message code="views.aaf.vhr.dashboard.nogroups"/></p>

commit 320f006b9935e030323782f8247f004cadabccaf

DashboardController: Corrects bugs introduced by a15f82d.
Ensures that group names are not duplicated on the dashboard and
corrects 3 failing test cases.

virtualhomeregistry/grails-app/controllers/aaf/vhr/DashboardController.groovy
@@ -66,7 +66,8 @@ class DashboardController {
       if(collatedGroupInstanceList."${it.organization.displayName}" == null) {
         collatedGroupInstanceList."${it.organization.displayName}" = [] as List
       }
-      collatedGroupInstanceList."${it.organization.displayName}" << it
+      if(!collatedGroupInstanceList."${it.organization.displayName}".contains(it))
+        collatedGroupInstanceList."${it.organization.displayName}" << it
     }
 
     [organizationInstanceList:organizationInstanceList, groupInstanceList: collatedGroupInstanceList, statistics:statistics]

virtualhomeregistry/test/unit/aaf/vhr/DashboardControllerSpec.groovy
@@ -113,13 +113,15 @@ class DashboardControllerSpec extends spock.lang.Specification {
 
     model.groupInstanceList != null
     model.groupInstanceList.size() == 1
-    model.groupInstanceList[0] == g
+    model.groupInstanceList."${g.organization.displayName}"[0] == g
   }
 
   def 'ensure subject administering multiple groups has correct data supplied'() {
     setup:
+    def organizationTestInstance = Organization.build()
+
     (1..10).each {
-      def g = Group.build()
+      def g = Group.build(organization:organizationTestInstance)
       def r = Role.build(name:"group:${g.id}:adminsters")
       
       subject.addToRoles(r)
@@ -139,7 +141,8 @@ class DashboardControllerSpec extends spock.lang.Specification {
     model.organizationInstanceList.size() == 0
 
     model.groupInstanceList != null
-    model.groupInstanceList.size() == 10
+    model.groupInstanceList.size() == 1
+    model.groupInstanceList."${organizationTestInstance.displayName}".size() == 10
   }
 
   def 'ensure statistics are correctly populated'() {
@@ -180,7 +183,6 @@ class DashboardControllerSpec extends spock.lang.Specification {
     model.organizationInstanceList.size() == 10
 
     model.groupInstanceList != null
-    model.groupInstanceList.size() == 10
 
     model.statistics.organizations == 50    // each 1..10 creates unique org
     model.statistics.groups == 30           // Managed Subject Build creates group plus 2 * 1..10 creating Groups