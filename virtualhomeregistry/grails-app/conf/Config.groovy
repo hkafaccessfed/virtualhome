e2f6124809681d3379f0e0cdd011f76f22745377
Bradley Beddoes <bradleybeddoes@gmail.com>
2013-02-19 11:51:06 +1000

    Config: Updated local configuration.
    Loading of external config needs to be in local config file as it is
    being ignored by Grails on start.
    
    Updates local config to ensure previous test environment isolation
    changes still apply.

--- virtualhomeregistry/grails-app/conf/Config.groovy
+++ virtualhomeregistry/grails-app/conf/Config.groovy
@@ -1,6 +1,27 @@
 import javax.naming.InitialContext
 import javax.naming.Context
 
+import grails.util.Environment
+import org.apache.log4j.FileAppender
+
+// Import externalized configuration
+if(Environment.current != Environment.TEST) {
+  def externalConf = getFromEnvironment("config_dir")
+  if(externalConf) {
+    grails.config.locations = ["file:${externalConf}/application_config.groovy"]
+  } else {
+    println "No external configuration location specified as environment variable config_dir, terminating startup"
+    throw new RuntimeException("No external configuration location specified as environment variable config_dir")
+  }
+}
+
+// Extract user details to append to Audit Table
+auditLog {
+  actorClosure = { request, session ->
+    org.apache.shiro.SecurityUtils.getSubject()?.getPrincipal()
+  }
+}
+
 security.shiro.authc.required = false
 
 grails.project.groupId = appName
@@ -43,5 +64,32 @@ environments {
     grails.mail.port = com.icegreen.greenmail.util.ServerSetupTest.SMTP.port
     grails.mail.default.from="noreply-test@aaf.edu.au"
     greenmail.disabled = false
+
+    log4j = {
+      appenders {
+        appender new FileAppender(name:"test-output", layout:pattern(conversionPattern: "%d{[ dd.MM.yy HH:mm:ss.SSS]} %-5p %c %x - %m%n"), file:"/tmp/app-test-output.log")
+      }
+      warn 'test-output'     :[ 'grails.buildtestdata'], additivity:false
+      info  'test-output'    :[ 'grails.app.controllers',
+                                'grails.app.domains',
+                                'grails.app.services',
+                                'grails.app.realms',
+                                'aaf.vhr']
+      } 
   }
+}
+
+/**
+* This is allows usage of environment variables in production
+* while maintaining flexibility in development.
+*/
+public String getFromEnvironment(final String name) {
+  if(name == null) return null;
+  try {
+    final Object object = ((Context)(new InitialContext().lookup("java:comp/env"))).lookup(name);
+    if (object != null)
+      return object.toString();
+  } catch (final Exception e) {}
+
+  System.getenv(name);
 }
