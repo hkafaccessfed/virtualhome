fd1b0cc785a581dd69b205038b065fcc4d411352
Bradley Beddoes <bradleybeddoes@gmail.com>
2013-04-30 16:07:46 +1000

    Deployment: Added and enhanced VH configuration files.

---
+++ configuration/vhr/bootstrap.groovy
@@ -0,0 +1,35 @@
+import aaf.base.identity.*
+import aaf.vhr.*
+
+/*
+  Configure basic AAF VH components for new install
+
+  Bradley Beddoes
+  April 2013
+*/
+  
+// Shibboleth IdP API Account
+  
+// Suggested generation $> openssl rand -base64 16 | tr -d '=+/\r\n'
+String principal = ''  
+
+// Suggested generation $> openssl rand -base64 16 | tr -d '=+/\r\n'
+// This must be kept a secret and known only to the API user
+String apiKey = ''
+
+
+
+
+
+// NO MORE CHANGES BELOW THIS POINT
+
+def apiSubject = new ApiSubject(principal:principal, apiKey:apiKey, enabled:true)
+if(!apiSubject.save()) {
+  apiSubject.errors.each { println it }
+  return false
+}
+
+def orgService = ctx.getBean('organizationService')
+orgService.populate()
+
+println "VH has been populated with ${Organization.count()} organizations from Federation Registry"
