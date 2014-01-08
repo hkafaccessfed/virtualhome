d5b2ad878fed5420bba8c326c69c94b874c2ef78
Bradley Beddoes <bradleybeddoes@gmail.com>
2014-01-09 09:33:25 +1000

    Login: Remove need to migrate users.
    As all users should now have been migrated from the old AAF VH
    which was provided by the switch federation this code is no longer
    required.

--- virtualhome/grails-app/controllers/aaf/vhr/LoginController.groovy
+++ virtualhome/grails-app/controllers/aaf/vhr/LoginController.groovy
@@ -44,13 +44,6 @@ class LoginController {
   }
 
   def login(String username, String password) {
-    def deprecatedSubject = DeprecatedSubject.findWhere(login:username, migrated:false)
-    if(deprecatedSubject) {
-      session.setAttribute(MigrateController.MIGRATION_USER, username)
-      redirect (controller:'migrate', action:'introduction')
-      return
-    }
-
     def redirectURL = session.getAttribute(SSO_URL)
     if(!redirectURL) {
       log.error "No redirectURL set for login, redirecting to oops"
