eb9dd597c99be7ef5f72c5388ceca9333a522916
Bradley Beddoes <bradleybeddoes@gmail.com>
2014-01-30 14:51:35 +1000

    API: Add additional API to support basic auth for VH.
    The ECP project work undertaken for NeCTAR revealed the need for the
    VH to support authentication for ECP requests/sessions. This was only
    possible in a practical sense using basic auth.
    
    This change introduces an API which will validate a supplied
    username/password pair for remote processes. The intedended recipient
    at this time is a filter within Virtual Home.

--- virtualhome/test/unit/aaf/vhr/api/v1/LoginApiControllerSpec.groovy
+++ virtualhome/test/unit/aaf/vhr/api/v1/LoginApiControllerSpec.groovy
@@ -9,10 +9,11 @@ import spock.lang.*
 import aaf.base.identity.*
 
 @TestFor(aaf.vhr.api.v1.LoginApiController)
-
+@Build([aaf.vhr.Organization, aaf.vhr.Group, aaf.vhr.ManagedSubject])
+@Mock([aaf.vhr.Organization, aaf.vhr.Group, aaf.vhr.ManagedSubject])
 class LoginApiControllerSpec extends spock.lang.Specification {
 
-  def "recieve 410 is there is no such session"() {
+  def "confirmsession: recieve 410 is there is no such session"() {
     setup:
     def loginService = Mock(aaf.vhr.LoginService)
     controller.loginService = loginService
@@ -27,7 +28,7 @@ class LoginApiControllerSpec extends spock.lang.Specification {
     response.status == 410
   }
 
-  def "recieve valid json when session is present"() {
+  def "confirmsession: recieve valid json when session is present"() {
     setup:
     def loginService = Mock(aaf.vhr.LoginService)
     controller.loginService = loginService
@@ -42,6 +43,66 @@ class LoginApiControllerSpec extends spock.lang.Specification {
     response.status == 200
     response.contentType == 'application/json;charset=UTF-8'
     response.text == '{"remote_user":"testuser"}'
+  }
+
+  def "basicauth: recieve 410 is there is no such object"() {
+    setup:
+    def cryptoService = Mock(aaf.vhr.CryptoService)
+    controller.cryptoService = cryptoService
+
+    when:
+    controller.basicauth("testuser", "password")
+
+    then:
+    response.status == 410
+  }
+
+  def "basicauth: recieve 403 if account not functioning"() {
+    setup:
+    def ms = aaf.vhr.ManagedSubject.build(active:false)
+    ms.organization.active = true
+
+    def cryptoService = Mock(aaf.vhr.CryptoService)
+    controller.cryptoService = cryptoService
+
+    when:
+    controller.basicauth(ms.login, "password")
+
+    then:
+    0 * cryptoService.verifyPasswordHash("password", ms)
+    response.status == 403
+  }
+
+  def "basicauth: recieve 403 if invalid credential supplied"() {
+    setup:
+    def ms = aaf.vhr.ManagedSubject.build(active:true)
+    ms.organization.active = true
+
+    def cryptoService = Mock(aaf.vhr.CryptoService)
+    controller.cryptoService = cryptoService
+
+    when:
+    controller.basicauth(ms.login, "password")
+
+    then:
+    1 * cryptoService.verifyPasswordHash("password", ms) >> false
+    response.status == 403
+  }
+
+  def "basicauth: recieve 200 if valid credential supplied"() {
+    setup:
+    def ms = aaf.vhr.ManagedSubject.build(active:true)
+    ms.organization.active = true
+
+    def cryptoService = Mock(aaf.vhr.CryptoService)
+    controller.cryptoService = cryptoService
+
+    when:
+    controller.basicauth(ms.login, "password")
+
+    then:
+    1 * cryptoService.verifyPasswordHash("password", ms) >> true
+    response.status == 200
   }
 
 }
