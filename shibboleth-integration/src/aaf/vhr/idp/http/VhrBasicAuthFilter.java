c8e176cd59199aac07dbe4e1b2734e0d383f0468
Bradley Beddoes <bradleybeddoes@gmail.com>
2014-01-30 14:58:53 +1000

    IdP: Support basic auth session establishment.
    This code introduces a filter and associated logic that utilises
    the Authorization header to establish a session (basic auth).
    
    This requires the VH deployment to have the previously commited API
    endpoint that supports basic auth.
    
    A simple test app per the original VH filter is also provided.

---
+++ shibboleth-integration/src/aaf/vhr/idp/http/VhrBasicAuthFilter.java
@@ -0,0 +1,79 @@
+package aaf.vhr.idp.http;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.codec.Charsets;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import aaf.vhr.idp.VhrBasicAuthValidator;
+
+public class VhrBasicAuthFilter implements Filter {
+
+  private String realm;
+  private VhrBasicAuthValidator vhrBasicAuthValidator;
+
+  Logger log = LoggerFactory.getLogger("aaf.vhr.idp.http.VhrFilter");
+
+  @Override
+  public void destroy() {
+  }
+
+  @Override
+  public void doFilter(ServletRequest req, ServletResponse res,
+      FilterChain chain) throws IOException, ServletException {
+
+    HttpServletRequest request = (HttpServletRequest) req;
+    HttpServletResponse response = (HttpServletResponse) res;
+
+    final String authorization = request.getHeader( "Authorization" );
+    if(authorization != null && authorization.contains(" ")) {
+      log.info("Attempting to establish session via Basic Auth");
+      log.debug("WWW-Authenticate: " + authorization);
+
+      final String[] credentials = StringUtils.split( new String( Base64.decodeBase64( authorization.substring( authorization.indexOf(" ") ) ), Charsets.UTF_8 ), ':' );
+
+      if ( credentials.length == 2 ) {
+    	final String login = credentials[0];
+    	final String password = credentials[1];
+        log.info ("Located basic authentication credentials for " + login + " validating password with VH.");
+        final String remoteUser = vhrBasicAuthValidator.authenticate(login, password);
+
+        if(remoteUser != null) {
+          log.info ("Confirmed supplied credentials for " + credentials[0] + ", VH confirmed remoteUser value of " + remoteUser);
+          VhrRequestWrapper vhrRequestWrapper = new VhrRequestWrapper(request, remoteUser);
+          chain.doFilter(vhrRequestWrapper, response);
+        }
+      } else {
+        log.info ("Invalid Authorization header detected when attempting to setup session");
+      }
+    }
+
+    response.setHeader( "WWW-Authenticate", "Basic realm=\"" + realm + "\"" );
+    response.sendError( HttpServletResponse.SC_UNAUTHORIZED );
+  }
+
+  @Override
+  public void init(FilterConfig filterConfig) throws ServletException {
+	realm = filterConfig.getInitParameter("realm");
+    String apiServer = filterConfig.getInitParameter("apiServer");
+    String apiEndpoint = filterConfig.getInitParameter("apiEndpoint");
+    String apiToken = filterConfig.getInitParameter("apiToken");
+    String apiSecret = filterConfig.getInitParameter("apiSecret");
+    String requestingHost = filterConfig.getInitParameter("requestingHost");
+
+    vhrBasicAuthValidator = new VhrBasicAuthValidator(apiServer, apiEndpoint, apiToken, apiSecret, requestingHost);
+  }
+
+}
